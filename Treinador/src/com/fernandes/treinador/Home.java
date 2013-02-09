1dcd6dfe4b04eefad48df6957ed0366e2dfba063
Ausmarton Zarino Fernandes <ausmarton@gmail.com>
2013-02-09 20:35:25 +0530

    Revert "ausmarton: added a simple button and text field which takes and returns the same string, also deleted the bin and gen files which somehow has gotten through last time."
    This reverts commit 5a4a4dad519b7613184da5e51036a5bdcf2d45d6.

--- Treinador/src/com/fernandes/treinador/Home.java
+++ Treinador/src/com/fernandes/treinador/Home.java
@@ -2,24 +2,13 @@ package com.fernandes.treinador;
 
 import android.os.Bundle;
 import android.app.Activity;
-//import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class Home extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//    	Intent intent = getIntent();
-//    	String verb = intent.getStringExtra("verb");
-//    	TextView textView = new TextView(this);
-//        textView.setTextSize(40);
-//        textView.setText(verb);
-//        setContentView(textView);
-    	
-    	super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
 
@@ -28,16 +17,6 @@ public class Home extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_home, menu);
         return true;
-    }
-    
-    public void conjugate(View view) {
-//    	Intent intent = new Intent(this, Home.class);
-    	EditText editText = (EditText) findViewById(R.id.verb);
-        String verb = editText.getText().toString();
-//        intent.putExtra("verb", verb);
-//        startActivity(intent);
-        TextView textView = (TextView) findViewById(R.id.conjugation);
-        textView.setText(verb);
     }
     
 }
